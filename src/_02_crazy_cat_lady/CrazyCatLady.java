518f12cecb5ab0d583355f18bda74014d16dfe39
League Student <league@iMac-17.attlocal.net>
2019-12-09 19:57:28 -0800

    worked on module 3

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -14,7 +14,15 @@ String input = JOptionPane.showInputDialog("how many cats do you have");
 		// 2. Convert their answer into an int
 int cats = Integer.parseInt(input);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+if(cats> 3) {
+	JOptionPane.showMessageDialog(null, "You are a crazy cat woman");
+}
+else if(cats<3 && cats>0) {
+	playVideo("https://www.youtube.com/watch?v=a_IA-8nQ4FY");
+}
+else if(cats==0) {
+	playVideo("https://www.youtube.com/watch?v=ZJT9CeEhM10");
+}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
