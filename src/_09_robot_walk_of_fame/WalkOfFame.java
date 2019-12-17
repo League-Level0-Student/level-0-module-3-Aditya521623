6799ef2e1457bd09e21c274dbf0965132f8d9dc4
League Student <league@iMac-22.attlocal.net>
2019-12-16 19:55:12 -0800

    learned how to use modulo

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -8,11 +8,29 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot rob = new Robot("mini");
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.moveTo(50, 300);
 		// 2. Make the robot draw a star shape. Hint: 144.
+	rob.setSpeed(200);	
+	
+
+
+
+for(int j= 0; j <10; j++) {
+	rob.penDown();
+for(int i = 0; i<5; i++) {
+	
+	 rob.move(30);
+ rob.turn(144);
+}
+rob.penUp();
+rob.setAngle(90);
+rob.move(50);
+rob.setAngle(0);
+}
+
 
 		// 3. Set the size of the star to 30.
 
---
+++ src/horoscopee.java
@@ -0,0 +1,9 @@
+
+public class horoscopee {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
